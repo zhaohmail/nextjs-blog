40c934e9522a07cc681deff0a97a93f0ae4f40e6
赵红 <zhaohong.wh@haier.com>
2024-03-05 17:47:59 +0800

    first-commit

---
+++ components/layout.js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Hong Zhao'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <img
+                src="/images/profile.png"
+                className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <div className={utilStyles.colorInherit}>{name}</div>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      <div className={styles.backToHome}>
+        <Link href="/">
+          ← Back to home
+        </Link>
+        <br />
+        <Link href="/posts/first-post">
+        first-post
+        </Link>
+      </div>
+      
+    </div>
+  )
+}
